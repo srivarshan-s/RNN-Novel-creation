fa54b616aa7722ae134778d9a1f2c17d8f44a204
Srivarshan <srivarshan02@gmail.com>
2021-11-22 22:23:47 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
+import time
 
 from generate_data import dataset, chars_from_ids, ids_from_chars
 from model import model, OneStep
@@ -36,3 +37,19 @@ one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
 # Export the model
 tf.saved_model.save(one_step_model, 'one_step')
 print("Model saved")
+
+# Run the model in a loop to generate text
+start = time.time()
+states = None
+# Initial character to start the model
+next_char = tf.constant(['ROMEO:'])
+result = [next_char]
+
+for n in range(1000):
+  next_char, states = one_step_model.generate_one_step(next_char, states=states)
+  result.append(next_char)
+
+result = tf.strings.join(result)
+end = time.time()
+print(result[0].numpy().decode('utf-8'), '\n\n' + '_'*80)
+print('\nRun time:', end - start)
